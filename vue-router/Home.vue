ebb1898b1cd2e4808a113654914e99e16f39cea2
jsj188508 <jsj188508@sina.com>
2018-05-03 18:23:07 +0800

    添加vue-cli使用方法“
    git commit -m 添加vue-cli使用方法“

---
+++ vue-router/Home.vue
@@ -0,0 +1,18 @@
+
+// Home.vue
+export default {
+    computed: {
+        username() {
+            // 我们很快就会看到 `params` 是什么
+            return this.$route.params.username
+        }
+    },
+    methods: {
+        goBack() {
+            window.history.length > 1 ?
+                this.$router.go(-1) :
+                this.$router.push('/')
+        }
+    }
+}
+// 现在，应用已经启动了！
