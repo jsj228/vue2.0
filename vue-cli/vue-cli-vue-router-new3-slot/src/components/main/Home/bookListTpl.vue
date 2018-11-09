b9830aa8648e4f0ca92f69d998281d692aeaaebd
jsj188508 <jsj188508@sina.com>
2018-11-10 01:01:56 +0800

    完善slotTpl模态框部分

--- vue-cli/vue-cli-vue-router-new3-slot/src/components/main/Home/bookListTpl.vue
+++ vue-cli/vue-cli-vue-router-new3-slot/src/components/main/Home/bookListTpl.vue
@@ -6,17 +6,24 @@
 		</header>
 		<dl>	
 			<!-- 使用$emit自定义onBookSelect事件,并在父组件中；以及传值书的book数据,-->
-			<dd v-for="book in books" @click="$emit('onBookSelect',book)">
+			<dd v-for="book in books" @click="toggleslotTpl">
 				<p v-text="book.imgsrc"></p>
 				<img :src="book.imgsrc" :alt="book.titile">
 				<b v-text="book.titile"></b>
 				<span v-for="author in book.authors" v-text="author"></span>
 			</dd>
 		</dl>
+		<p>{{isShowSlot}}</p>
+		<slotTpl-v v-if="isShowSlot">
+			<div slot="header">slotHeader部分  <span style="float:right;" @click="toggleslotTpl">&times;</span></div>
+			<div >slotHeader插槽的内容</div>
+		</slotTpl-v>		
 	</div>
 </template>
 <script>
-//引入banner等局部组件模板
+//引入局部组件模板
+import slotTpl from "./slotTpl";
+
 export default {
 	name: 'bookListTpl',
 	props:[    //使用props来接受父组件传过来的数据
@@ -27,6 +34,19 @@ export default {
 		join(args){
 			return args.join(',')
 		}
+	},
+	data(){
+		return {
+			isShowSlot:false,
+		}
+	},methods:{
+		toggleslotTpl(){
+			this.isShowSlot=!this.isShowSlot;
+			console.log('0000',this.isShowSlot)
+		}
+	},
+	components:{
+		'slotTpl-v':slotTpl,
 	}
 }
 </script>
--- vue-cli/vue-cli-vue-router-new3-slot/src/components/main/Home/slotTpl.vue
+++ vue-cli/vue-cli-vue-router-new3-slot/src/components/main/Home/slotTpl.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="dialog" :class="{'open':is_open}">
-    <span @click="close">&times;</span>
+  <div class="dialog">
     <header>
       <!-- 头部信息 ;① 使用slot进行插槽时使用name属性进行命名 ②在HomeTpl父组件中引用-->
       <slot name="header"></slot>
@@ -18,30 +17,18 @@ export default {
   data () {
     return {
       msg:"这里是slot模板",
-      is_open:false,
+
     }
   },
   methods:{
-    open(){//打开事件
-      if(!this.is_open){
-        this.$emit('slotOpen')
-      }
-      this.is_open=true;
-    },
-    close(){
-      if(this.is_open){
-        this.$emit('slotClose')
-      }
-      this.is_open=false;
-    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.dialog{padding:10px;position: absolute;top:24px;left:24px;bottom:24px;right:24px;
-;z-index:100;box-show:0 0 10px rbga(0,0,0,0.5);background:#eee;display:none;}
-.dialog.open{display:block;}
+.dialog{padding:10px;position:fixed;top:24px;left:24px;bottom:24px;right:24px;
+;z-index:100;box-show:0 0 10px rbga(0,0,0,0.5);background:#eee;}
+
 
 </style>
